63a79e50d25bc8ad0be5805f08aaab18717cd024
Szilamer007 <ochuleta@gmail.com>
2023-02-26 20:33:28 +0100

    Delete everything that is save as GPX related

--- RangemanSync/RangemanSync/ViewModels/Download/MainPageViewModel.cs
+++ RangemanSync/RangemanSync/ViewModels/Download/MainPageViewModel.cs
@@ -3,16 +3,13 @@ using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 using RangemanSync.Services;
 using RangemanSync.Services.WatchDataReceiver;
-using RangemanSync.Services.WatchDataReceiver.DataExtractors.Data;
 using System.Collections.ObjectModel;
-using SharpGPX;
 
 namespace RangemanSync.ViewModels.Download
 {
     public partial class MainPageViewModel : BaseViewModel
     {
         public IAsyncRelayCommand DownloadHeadersCommand { get; }
-        public IAsyncRelayCommand SaveGPXCommand { get; }
         public IAsyncRelayCommand DisconnectCommand { get; set; }
 
         public ObservableCollection<LogHeaderViewModel> LogHeaderList { get; } = new ObservableCollection<LogHeaderViewModel>();
@@ -27,7 +24,6 @@ namespace RangemanSync.ViewModels.Download
             this.saveFileService = saveFileService;
             this.progressMessagesService = progressMessagesService;
             DownloadHeadersCommand = new AsyncRelayCommand(DownloadHeaders_Clicked);
-            SaveGPXCommand = new AsyncRelayCommand(DownloadSaveGPXButton_Clicked);
             DisconnectCommand = new AsyncRelayCommand(DisconnectButton_Clicked);
 
             progressMessage = progressMessagesService.InitalStartMessage;
@@ -100,79 +96,6 @@ namespace RangemanSync.ViewModels.Download
 
         }
 
-        private async Task DownloadSaveGPXButton_Clicked()
-        {
-            logger.LogInformation("--- MainPage - start DownloadSaveGPXButton_Clicked");
-
-            if (SelectedLogHeader == null)
-            {
-                logger.LogDebug("DownloadSaveGPXButton_Clicked : One log header entry should be selected");
-                SetProgressMessage(progressMessagesService.PleaseSelectLogHeader);
-                return;
-            }
-
-            var timeElapsedSinceLastHeaderDownloadTime = DateTime.Now - lastHeaderDownloadTime;
-
-            if (timeElapsedSinceLastHeaderDownloadTime.TotalMinutes > 30)
-            {
-                logger.LogDebug($"--- Old header data detected. Elapsed minutes = {timeElapsedSinceLastHeaderDownloadTime}");
-                SetProgressMessage(progressMessagesService.ThirtyMinutesOldHeader);
-                return;
-            }
-
-            SetProgressMessage(progressMessagesService.PleaseConnectWatch);
-
-            saveGPXButtonCanBePressed = false;
-            DisableOtherTabs();
-
-            await bluetoothConnectorService.FindAndConnectToWatch(SetProgressMessage, async (connection) =>
-            {
-                logger.LogDebug("DownloadSaveGPXButton_Clicked : Before GetLogDataAsync");
-                logger.LogDebug($"Selected ordinal number: {SelectedLogHeader.OrdinalNumber}");
-                var logPointMemoryService = new LogPointMemoryExtractorService(connection, loggerFactory);
-                logPointMemoryService.ProgressChanged += LogPointMemoryService_ProgressChanged;
-                var selectedHeader = SelectedLogHeader;
-                var logDataEntries = await logPointMemoryService.GetLogDataAsync(
-                    selectedHeader.DataSize,
-                    selectedHeader.DataCount,
-                    selectedHeader.LogAddress,
-                    selectedHeader.LogTotalLength);
-
-                logPointMemoryService.ProgressChanged -= LogPointMemoryService_ProgressChanged;
-
-                if (logDataEntries != null)
-                {
-                    logger.LogDebug("-- Inside DownloadSaveAsGPXButton: logDataEntries is not null! Calling SaveGPXFile()");
-                    await bluetoothConnectorService.DisconnectFromWatch(SetProgressMessage);
-                    SaveGPXFile(logDataEntries);
-                }
-                else
-                {
-                    ProgressMessage = progressMessagesService.TransmissionEndedWithoutReceivingAllData;
-                    logger.LogDebug("-- Inside DownloadSaveAsGPXButton: logDataEntries is null");
-                }
-
-                DisconnectButtonIsVisible = false;
-                WatchCommandButtonsAreVisible = true;
-
-                return true;
-            },
-            async () =>
-            {
-                SetProgressMessage(progressMessagesService.WatchCommandSendingError);
-                return true;
-            },
-            () => 
-            { 
-                DisconnectButtonIsVisible = true;
-                WatchCommandButtonsAreVisible = false;
-            });
-
-            EnableOtherTabs();
-            saveGPXButtonCanBePressed = true;
-            //Save selected log header as GPX
-        }
-
         private async Task DisconnectButton_Clicked()
         {
             await bluetoothConnectorService.DisconnectFromWatch(SetProgressMessage);
@@ -189,44 +112,6 @@ namespace RangemanSync.ViewModels.Download
         {
             //TODO: check implementation in MAUI
         }
-
-        private void SaveGPXFile(List<LogData> logDataEntries)
-        {
-            GpxClass gpx = new GpxClass();
-
-            gpx.Metadata.time = SelectedLogHeader.HeaderTime;
-            gpx.Metadata.timeSpecified = true;
-            gpx.Metadata.desc = "Track exported from Casio GPR-B1000 watch";
-
-            gpx.Tracks.Add(new SharpGPX.GPX1_1.trkType());
-            gpx.Tracks[0].trkseg.Add(new SharpGPX.GPX1_1.trksegType());
-
-            foreach (var logEntry in logDataEntries)
-            {
-                var wpt = new SharpGPX.GPX1_1.wptType
-                {
-                    lat = (decimal)logEntry.Latitude,
-                    lon = (decimal)logEntry.Longitude,   // ele tag : pressure -> elevation conversion ?
-                    time = logEntry.Date,
-                    timeSpecified = true,
-                };
-
-                gpx.Tracks[0].trkseg[0].trkpt.Add(wpt);
-            }
-
-            var headerTime = SelectedLogHeader.HeaderTime;
-            var fileName = $"GPR-B1000-Route-{headerTime.Year}-{headerTime.Month}-{headerTime.Day}-2.gpx";
-
-            var gpxString = gpx.ToXml();
-#if WINDOWS
-            saveFileService.SaveFile(fileName, gpxString);
-            SetProgressMessage(progressMessagesService.GPXSuccessfullySaved);
-#else
-            Preferences.Default.Set(Constants.PrefKeyGPX, gpxString);
-            saveFileService.SaveFile(fileName);
-#endif
-        }
-
 
         private void LogPointMemoryService_ProgressChanged(object sender, DataReceiverProgressEventArgs e)
         {
